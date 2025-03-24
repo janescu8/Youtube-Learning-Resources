6115246b4c97c1dc6f28bc58a755926f2f47436d
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-25 01:08:54 +0800

    Update link.py

--- link.py
+++ link.py
@@ -8,13 +8,31 @@ data = {
         {"title": "Streamlit & Google Sheets: The Easiest Database", "url": "https://youtu.be/HwxrXnYVIlU?feature=shared"},
         {"title": "Create a Google Sheets Data Entry Form with Python & Streamlit | Quick & Easy Tutorial", "url": "https://youtu.be/_G5f7og_Dpo?feature=shared"},
     ],
-    "Procreate | 0329": [
+    "Procreate | 0400": [
         {"title": "Procreate Beginners Series: Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
         {"title": "Procreate Beginners Series: Part Two | Painting Tools", "url": "https://youtu.be/e5F7em-hGwc?feature=shared"},
         {"title": "Procreate Beginners Series: Part Three | Editing Tools", "url": "https://youtu.be/EqQjjxcNd1o?feature=shared"},
         {"title": "Procreate Beginners Series: Part Four | Actions & Animation", "url": "https://youtu.be/Np97KSzgrCs?feature=shared"},
     ],
-    "Lucas | A1 level - beginner | Part 1 of 4 | 0401": [
+    "Procreate | 0400": [
+        {"title": "A REAL Procreate Beginner Tutorial 🍉 | Draw as a Professional Artist", "url": "https://youtu.be/X9PTqz4GSTg?feature=shared"},
+        {"title": "PROCREATE Drawing for Beginners - EASY Step by Step Tutorial", "url": "https://youtu.be/AX84Qc0dwo4?feature=shared"},
+        {"title": "How To Draw Watercolor Succulents In Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/EHYdVUZZuvg?feature=shared"},
+        {"title": "How To Draw An Apple • Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/w39cxJnU790?feature=shared"},
+        {"title": "How To Draw: Watercolor Mushroom • Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/5259hXGaLjo?feature=shared"},
+        {"title": "How To Draw A Watercolor Cactus In Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/PtiypxK6qcU?feature=shared"},
+        {"title": "Easy Watercolor Effect on iPad • No Art Skill Required • Step-By-Step Procreate Tutorial", "url": "https://youtu.be/6c2xiYOlJnw?feature=shared"},
+    ],
+    "Procreate | 0400": [
+        {"title": "Watercolor Monstera Leaf | Easy Procreate Tutorial | Step by Step (with voice!)", "url": "https://youtu.be/Zqob9gGOtys?feature=shared"},
+        {"title": "How To Draw A Watercolor Orange Slice In Procreate | Beginner Friendly | Step-by-Step Tutorial", "url": "https://youtu.be/UJTYznYPPJQ?feature=shared"},
+        {"title": "How To Draw Watercolor Fall Leaves • Procreate Tutorial • You Can Paint This iPad", "url": "https://youtu.be/ImVxjKS7I-g?feature=shared"},
+        {"title": "How To Draw: Watercolor Dream Catcher • Procreate Tutorial • Easy iPad Art", "url": "https://youtu.be/ryiWIwaT8M0?feature=shared"},
+        {"title": "how i illustrate using procreate⭐️(brushes + techniques)", "url": "https://youtu.be/FZC3LznrCBE?feature=shared"},
+        {"title": "The Boy step-by-step Procreate", "url": "https://youtu.be/BgK5iM_wHA4?feature=shared"},
+        {"title": "Learn How To Draw A Kawaii Monster 🤍 (Easy & Fun)", "url": "https://youtu.be/4vahtpSVdQ0?feature=shared"},
+    ],
+    "Lucas | A1 level - beginner | Part 1 of 4 | 0400": [
         {"title": "A1#1 Introduction", "url": "https://youtu.be/c2SUQVjklVA?feature=shared"},
         {"title": "A1#2 En Suisse", "url": "https://youtu.be/3ulVBcPpydk?feature=shared"},
         {"title": "A1#3 J'aime bien...", "url": "https://youtu.be/in8ORZKfSPk?feature=shared"},
@@ -22,7 +40,7 @@ data = {
         {"title": "A1#5 Une routine", "url": "https://youtu.be/5MxUsevYKzA?feature=shared"},
         {"title": "A1#6 Révision de 10 verbes", "url": "https://youtu.be/QPgEzFucZHo?feature=shared"},
     ],
-    "Lucas | A1 level - beginner | Part 2 of 4 | 0405": [
+    "Lucas | A1 level - beginner | Part 2 of 4 | 0400": [
         {"title": "A1#7 Vous préférez lire ou écouter?", "url": "https://youtu.be/21nGI69WyCA?feature=shared"},
         {"title": "A1#9 Conversation informelle", "url": "https://youtu.be/Yc3KB75SqIg?feature=shared"},
         {"title": "A1#10 5 adjectifs + 5 contraires", "url": "https://youtu.be/2bzCXZqGgwU?feature=shared"},
