a4ea2997000580efea8da9fe00df40031ae3f84c
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-24 22:02:26 +0800

    Update link.py

--- link.py
+++ link.py
@@ -3,5 +3,9 @@
 data = {
     "Streamlit": [
         {"title": "Streamlit Mini Course - Make Websites With ONLY Python", "url": "https://youtu.be/o8p7uQCGD0U?feature=shared"},
+        {"title": "How To Add A Contact Form To Your Streamlit App", "url": "https://youtu.be/FOULV9Xij_8?feature=shared"},
+        {"title": "Build A Digital Resume Using Python & Streamlit", "url": "https://youtu.be/BXAeMICmUSQ?feature=shared"},
+        {"title": "Streamlit & Google Sheets: The Easiest Database", "url": "https://youtu.be/HwxrXnYVIlU?feature=shared"},
+        {"title": "Create a Google Sheets Data Entry Form with Python & Streamlit | Quick & Easy Tutorial", "url": "https://youtu.be/_G5f7og_Dpo?feature=shared"},
     ],
 }
