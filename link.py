9d0d576e6214d005f671590f7a72381533f339a5
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-25 00:38:01 +0800

    Update link.py

--- link.py
+++ link.py
@@ -1,6 +1,19 @@
 # link.py
 
 data = {
+    "Lucas | A1 level - beginner | Part 1 of 2": [
+        {"title": "A1#1 Introduction", "url": "https://youtu.be/c2SUQVjklVA?feature=shared"},
+        {"title": "A1#2 En Suisse", "url": "https://youtu.be/3ulVBcPpydk?feature=shared"},
+        {"title": "A1#3 J'aime bien...", "url": "https://youtu.be/in8ORZKfSPk?feature=shared"},
+        {"title": "A1#4 Vous aimez ce sportif ?", "url": "https://youtu.be/OezZ0xue_yI?feature=shared"},
+        {"title": "A1#5 Une routine", "url": "https://youtu.be/5MxUsevYKzA?feature=shared"},
+        {"title": "A1#6 Révision de 10 verbes", "url": "https://youtu.be/QPgEzFucZHo?feature=shared"},
+        {"title": "A1#7 Vous préférez lire ou écouter?", "url": "https://youtu.be/21nGI69WyCA?feature=shared"},
+        {"title": "A1#9 Conversation informelle", "url": "https://youtu.be/Yc3KB75SqIg?feature=shared"},
+        {"title": "A1#10 5 adjectifs + 5 contraires", "url": "https://youtu.be/2bzCXZqGgwU?feature=shared"},
+        {"title": "A1#11 Chiens ou chats?", "url": "https://youtu.be/IB7eYfhP1BU?feature=shared"},
+        {"title": "A1#12 Trois personnes célèbres" , "url": "https://youtu.be/vjtM9uTNF9U?feature=shared"},
+    ],
     "Streamlit": [
         {"title": "Streamlit Mini Course - Make Websites With ONLY Python", "url": "https://youtu.be/o8p7uQCGD0U?feature=shared"},
         {"title": "How To Add A Contact Form To Your Streamlit App", "url": "https://youtu.be/FOULV9Xij_8?feature=shared"},
