3e0000ea16598e61d16c84cc8e1651e43d0af1fc
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-25 00:43:42 +0800

    Update link.py

--- link.py
+++ link.py
@@ -1,7 +1,20 @@
 # link.py
 
 data = {
-    "Lucas | A1 level - beginner | Part 1 of 4": [
+    "Streamlit | 0325": [
+        {"title": "Streamlit Mini Course - Make Websites With ONLY Python", "url": "https://youtu.be/o8p7uQCGD0U?feature=shared"},
+        {"title": "How To Add A Contact Form To Your Streamlit App", "url": "https://youtu.be/FOULV9Xij_8?feature=shared"},
+        {"title": "Build A Digital Resume Using Python & Streamlit", "url": "https://youtu.be/BXAeMICmUSQ?feature=shared"},
+        {"title": "Streamlit & Google Sheets: The Easiest Database", "url": "https://youtu.be/HwxrXnYVIlU?feature=shared"},
+        {"title": "Create a Google Sheets Data Entry Form with Python & Streamlit | Quick & Easy Tutorial", "url": "https://youtu.be/_G5f7og_Dpo?feature=shared"},
+    ],
+    "Procreate | 0329": [
+        {"title": "Procreate Beginners Series: Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
+        {"title": "Procreate Beginners Series: Part Two | Painting Tools", "url": "https://youtu.be/e5F7em-hGwc?feature=shared"},
+        {"title": "Procreate Beginners Series: Part Three | Editing Tools", "url": "https://youtu.be/EqQjjxcNd1o?feature=shared"},
+        {"title": "Procreate Beginners Series: Part Four | Actions & Animation", "url": "https://youtu.be/Np97KSzgrCs?feature=shared"},
+    ],
+    "Lucas | A1 level - beginner | Part 1 of 4 | 0401": [
         {"title": "A1#1 Introduction", "url": "https://youtu.be/c2SUQVjklVA?feature=shared"},
         {"title": "A1#2 En Suisse", "url": "https://youtu.be/3ulVBcPpydk?feature=shared"},
         {"title": "A1#3 J'aime bien...", "url": "https://youtu.be/in8ORZKfSPk?feature=shared"},
@@ -9,24 +22,11 @@ data = {
         {"title": "A1#5 Une routine", "url": "https://youtu.be/5MxUsevYKzA?feature=shared"},
         {"title": "A1#6 Révision de 10 verbes", "url": "https://youtu.be/QPgEzFucZHo?feature=shared"},
     ],
-    "Lucas | A1 level - beginner | Part 2 of 4": [
+    "Lucas | A1 level - beginner | Part 2 of 4 | 0405": [
         {"title": "A1#7 Vous préférez lire ou écouter?", "url": "https://youtu.be/21nGI69WyCA?feature=shared"},
         {"title": "A1#9 Conversation informelle", "url": "https://youtu.be/Yc3KB75SqIg?feature=shared"},
         {"title": "A1#10 5 adjectifs + 5 contraires", "url": "https://youtu.be/2bzCXZqGgwU?feature=shared"},
         {"title": "A1#11 Chiens ou chats?", "url": "https://youtu.be/IB7eYfhP1BU?feature=shared"},
         {"title": "A1#12 Trois personnes célèbres" , "url": "https://youtu.be/vjtM9uTNF9U?feature=shared"},
-    ],
-    "Streamlit": [
-        {"title": "Streamlit Mini Course - Make Websites With ONLY Python", "url": "https://youtu.be/o8p7uQCGD0U?feature=shared"},
-        {"title": "How To Add A Contact Form To Your Streamlit App", "url": "https://youtu.be/FOULV9Xij_8?feature=shared"},
-        {"title": "Build A Digital Resume Using Python & Streamlit", "url": "https://youtu.be/BXAeMICmUSQ?feature=shared"},
-        {"title": "Streamlit & Google Sheets: The Easiest Database", "url": "https://youtu.be/HwxrXnYVIlU?feature=shared"},
-        {"title": "Create a Google Sheets Data Entry Form with Python & Streamlit | Quick & Easy Tutorial", "url": "https://youtu.be/_G5f7og_Dpo?feature=shared"},
-    ],
-    "Procreate": [
-        {"title": "Procreate Beginners Series: Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Two | Painting Tools", "url": "https://youtu.be/e5F7em-hGwc?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Three | Editing Tools", "url": "https://youtu.be/EqQjjxcNd1o?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Four | Actions & Animation", "url": "https://youtu.be/Np97KSzgrCs?feature=shared"},
     ],
 }
