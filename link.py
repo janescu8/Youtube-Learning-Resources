608b19218c0f52ec331774540166a4b5942f3e49
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-29 21:21:11 +0800

    Update link.py

--- link.py
+++ link.py
@@ -7,71 +7,10 @@ data = {
     "Git & GitHub | Part 2 of 2 | 0400": [
         {"title": "Git & GitHub Tutorial for Beginners", "url": "https://youtube.com/playlist?list=PL4cUxeGkcC9goXbgTDQ0n_4TBzOO0ocPR&feature=shared"},
     ],
-    "Math for Machine Learning | Part 1 of 3 | 0400": [
+    "Math for Machine Learning | Part 1 of 1 | 0400": [
         {"title": "政大【數學軟體應用】(深度學習) 課程", "url": "https://youtube.com/playlist?list=PLpltJwWB6egLxrYImtGdqy_FV3sJe5dmy&feature=shared"},
-    ],
-    "Math for Machine Learning | Part 2 of 3 | 0400": [
-        {"title": "Math for Machine Learning", "url": "https://youtube.com/playlist?list=PLD80i8An1OEGZ2tYimemzwC3xqkU0jKUg&feature=shared"},
-    ],
-    "Math for Machine Learning | Part 3 of 3 | 0400": [
-        {"title": "Math for Deep Learning — Andreas Geiger", "url": "https://youtube.com/playlist?list=PL05umP7R6ij0bo4UtMdzEJ6TiLOqj4ZCm&feature=shared"},
-    ],
-    "AI | Part 1 of 2 | 0400": [
-        {"title": "Google’s AI Course for Beginners (in 10 minutes)!", "url": "https://youtu.be/Yq0QkCxoTHM?feature=shared"},
-        {"title": "99% of Beginners Don't Know the Basics of AI", "url": "https://youtu.be/nVyD6THcvDQ?feature=shared"},
-        {"title": "How I'd Learn AI in 2025 (if I could start over)", "url": "https://youtu.be/h2FDq3agImI?feature=shared"},
-        {"title": "How I'd Learn AI (If I Had to Start Over)", "url": "https://youtu.be/TR7AGmey1C8?feature=shared"},
-    ],
-    "AI | Part 2 of 2 | 0400": [
-        {"title": "Basics of ChatGPT & Prompt Engineering for Beginners", "url": "https://youtube.com/playlist?list=PLlHtucAD9KT3mCqOBIrJwO2rsfia_wj1U&feature=shared"},
     ],
     "Midjourney | Part 1 of 1 | 0400": [
         {"title": "The ULTIMATE Beginners Guide to Midjourney in 2025", "url": "https://youtu.be/vUj4VNXXC1c?feature=shared"},
-    ],
-    "Procreate | Part 1 of 3 | 0400": [
-        {"title": "Procreate Beginners Series: Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Two | Painting Tools", "url": "https://youtu.be/e5F7em-hGwc?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Three | Editing Tools", "url": "https://youtu.be/EqQjjxcNd1o?feature=shared"},
-        {"title": "Procreate Beginners Series: Part Four | Actions & Animation", "url": "https://youtu.be/Np97KSzgrCs?feature=shared"},
-    ],
-    "Procreate | Part 2 of 3 | 0400": [
-        {"title": "A REAL Procreate Beginner Tutorial 🍉 | Draw as a Professional Artist", "url": "https://youtu.be/X9PTqz4GSTg?feature=shared"},
-        {"title": "PROCREATE Drawing for Beginners - EASY Step by Step Tutorial", "url": "https://youtu.be/AX84Qc0dwo4?feature=shared"},
-        {"title": "How To Draw Watercolor Succulents In Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/EHYdVUZZuvg?feature=shared"},
-        {"title": "How To Draw An Apple • Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/w39cxJnU790?feature=shared"},
-        {"title": "How To Draw: Watercolor Mushroom • Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/5259hXGaLjo?feature=shared"},
-        {"title": "How To Draw A Watercolor Cactus In Procreate • Easy iPad Art Tutorial", "url": "https://youtu.be/PtiypxK6qcU?feature=shared"},
-        {"title": "Easy Watercolor Effect on iPad • No Art Skill Required • Step-By-Step Procreate Tutorial", "url": "https://youtu.be/6c2xiYOlJnw?feature=shared"},
-    ],
-    "Procreate | Part 3 of 3 | 0400": [
-        {"title": "Watercolor Monstera Leaf | Easy Procreate Tutorial | Step by Step (with voice!)", "url": "https://youtu.be/Zqob9gGOtys?feature=shared"},
-        {"title": "How To Draw A Watercolor Orange Slice In Procreate | Beginner Friendly | Step-by-Step Tutorial", "url": "https://youtu.be/UJTYznYPPJQ?feature=shared"},
-        {"title": "How To Draw Watercolor Fall Leaves • Procreate Tutorial • You Can Paint This iPad", "url": "https://youtu.be/ImVxjKS7I-g?feature=shared"},
-        {"title": "How To Draw: Watercolor Dream Catcher • Procreate Tutorial • Easy iPad Art", "url": "https://youtu.be/ryiWIwaT8M0?feature=shared"},
-        {"title": "how i illustrate using procreate⭐️(brushes + techniques)", "url": "https://youtu.be/FZC3LznrCBE?feature=shared"},
-        {"title": "The Boy step-by-step Procreate", "url": "https://youtu.be/BgK5iM_wHA4?feature=shared"},
-        {"title": "Learn How To Draw A Kawaii Monster 🤍 (Easy & Fun)", "url": "https://youtu.be/4vahtpSVdQ0?feature=shared"},
-    ],
-    "Suno | Part 1 of 1 | 0400": [
-        {"title": "Suno AI: Everything You Need to Know (Ultimate Guide)", "url": "https://youtu.be/HEsLVSN7aPo?feature=shared"},
-    ],
-    "REELS | Part 1 of 1 | 0400": [
-        {"title": "INSTAGRAM REELS TUTORIAL 2025 | How to Create 20 Reels in 1 hour", "url": "https://youtu.be/1FfxkrCoTOw?feature=shared"},
-        {"title": "2025 Reels Editing Tutorial for Beginners (How to Edit Reels in the Instagram App)", "url": "https://youtu.be/7rgFWbwvKiE?feature=shared"},
-    ],
-    "Lucas | A1 level - beginner | Part 1 of 4 | 0400": [
-        {"title": "A1#1 Introduction", "url": "https://youtu.be/c2SUQVjklVA?feature=shared"},
-        {"title": "A1#2 En Suisse", "url": "https://youtu.be/3ulVBcPpydk?feature=shared"},
-        {"title": "A1#3 J'aime bien...", "url": "https://youtu.be/in8ORZKfSPk?feature=shared"},
-        {"title": "A1#4 Vous aimez ce sportif ?", "url": "https://youtu.be/OezZ0xue_yI?feature=shared"},
-        {"title": "A1#5 Une routine", "url": "https://youtu.be/5MxUsevYKzA?feature=shared"},
-        {"title": "A1#6 Révision de 10 verbes", "url": "https://youtu.be/QPgEzFucZHo?feature=shared"},
-    ],
-    "Lucas | A1 level - beginner | Part 2 of 4 | 0400": [
-        {"title": "A1#7 Vous préférez lire ou écouter?", "url": "https://youtu.be/21nGI69WyCA?feature=shared"},
-        {"title": "A1#9 Conversation informelle", "url": "https://youtu.be/Yc3KB75SqIg?feature=shared"},
-        {"title": "A1#10 5 adjectifs + 5 contraires", "url": "https://youtu.be/2bzCXZqGgwU?feature=shared"},
-        {"title": "A1#11 Chiens ou chats?", "url": "https://youtu.be/IB7eYfhP1BU?feature=shared"},
-        {"title": "A1#12 Trois personnes célèbres" , "url": "https://youtu.be/vjtM9uTNF9U?feature=shared"},
     ],
 }
