7607eee932725264df8a4b8d65ac0bfbcff93eb7
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-03-30 20:53:15 +0800

    Update link.py

--- link.py
+++ link.py
@@ -7,9 +7,6 @@ data = {
     "Git & GitHub | Part 2 of 2 | 0400": [
         {"title": "Git & GitHub Tutorial for Beginners", "url": "https://youtube.com/playlist?list=PL4cUxeGkcC9goXbgTDQ0n_4TBzOO0ocPR&feature=shared"},
     ],
-    "Math for Machine Learning | Part 1 of 1 | 0400": [
-        {"title": "政大【數學軟體應用】(深度學習) 課程", "url": "https://youtube.com/playlist?list=PLpltJwWB6egLxrYImtGdqy_FV3sJe5dmy&feature=shared"},
-    ],
     "Midjourney | Part 1 of 1 | 0400": [
         {"title": "The ULTIMATE Beginners Guide to Midjourney in 2025", "url": "https://youtu.be/vUj4VNXXC1c?feature=shared"},
     ],
