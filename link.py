44cac392abe5245ad506be4144c72d666eb53f22
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-04-16 16:42:49 +0800

    Update link.py

--- link.py
+++ link.py
@@ -1,24 +1,6 @@
 # link.py
 
 data = {
-    "Forum | Part 1 of 6 | 0400": [
-        {"title": "How To Create A Discussion Forum Website With HTML, CSS, And JavaScript (Frontend Tutorial 2021)", "url": "https://youtu.be/knGk9aUr4Do?feature=shared"},
-    ],
-    "Forum | Part 2 of 6 | 0400": [
-        {"title": "How To Build A Forum Website From Scratch Using Django - Django Project 2021 [Part 1]", "url": "https://youtu.be/YXmsi13cMhw?feature=shared"},
-    ],
-    "Forum | Part 3 of 6 | 0400": [
-        {"title": "How To Build A Forum Website From Scratch Using Django - Django Authentication System [Part 2]", "url": "https://youtu.be/Q6bnozjEIuU?feature=shared"},
-    ],
-    "Forum | Part 4 of 6 | 0400": [
-        {"title": "How To Build A Forum Website From Scratch Using Django - Django Comment System [Part 3]", "url": "https://youtu.be/6SkoqRJ-yAw?feature=shared"},
-    ],
-    "Forum | Part 5 of 6 | 0400": [
-        {"title": "How To Build A Forum Website From Scratch Using Django - Django Search Form [Part 4]", "url": "https://youtu.be/2zNua09rBjM?feature=shared"},
-    ],
-    "Forum | Part 6 of 6 | 0400": [
-        {"title": "How To Build A Forum Website From Scratch Using Django - Deploy To Heroku For Free [Part 4]", "url": "https://youtu.be/EYLuxdW-gzQ?feature=shared"},
-    ],
     "Git & GitHub | Part 1 of 2 | 0400": [
         {"title": "Git Tutorial for Beginners: Learn Git in 1 Hour", "url": "https://youtu.be/8JJ101D3knE?feature=shared"},
     ],
