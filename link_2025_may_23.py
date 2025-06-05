7f752f6235f34492d8d5b9f44f77ad96fe5d5770
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-06 00:25:08 +0800

    Update link_2025_may_23.py

--- link_2025_may_23.py
+++ link_2025_may_23.py
@@ -1,55 +1,11 @@
 # link.py
 
 data = {
-    "3 days of Art Production Scottie": [
-        {"title": "Day 1 – Sketching PEOPLE SITTING", "url": "https://youtu.be/vKc62VakGxo?feature=shared"},
-        {"title": "Day 2 – ", "url": ""},
-        {"title": "Day 3 – ", "url": ""},
-    ],
-    "4 days of Procreate Production Christiane": [
+    "1 days of Procreate Production Christiane": [
         {"title": "Day 1 – Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
-        {"title": "Day 2 – Part Two | Painting Tools", "url": "https://youtu.be/e5F7em-hGwc?feature=shared"},
-        {"title": "Day 3 – Part Three | Editing Tools", "url": "https://youtu.be/EqQjjxcNd1o?feature=shared"},
-        {"title": "Day 4 – Part Four | Actions & Animation", "url": "https://youtu.be/Np97KSzgrCs?feature=shared"},
     ],
-    "3 days of Piano Production Thomas": [
+    "2 days of Piano Production Thomas": [
         {"title": "Day 1 – GOLDEN HOUR", "url": "https://youtu.be/CWlAgyg25OA?feature=shared"},
         {"title": "Day 2 – GOLDEN HOUR", "url": "https://youtu.be/ksZRx9k18Yw?feature=shared"},
-        {"title": "Day 3 – COMPTINE D'UN AUTRE ÉTÉ", "url": "https://youtu.be/2FZGQ76RbK4?feature=shared"},
-    ],
-    "2 days of Dance Production Lara": [
-        {"title": "Day 1 – HIP HOP Dance Workout", "url": "https://youtu.be/KHzdmT-irdM?feature=shared"},
-        {"title": "Day 2 – This HIP HOP Dance Tutorial in 2024", "url": "https://youtu.be/8ln8T3yQ9bU?feature=shared"},
-    ],
-    "1 days of Dance Production Tara's body": [
-        {"title": "Day 1 – DANCE TUTORIAL & CHOREGRAPHY | FOR BEGINNERS", "url": "https://youtu.be/gbfvCYz9i9Y?feature=shared"},
-    ],
-    "3 days of Barre Production Lara": [
-        {"title": "Day 1 – LOWER BODY BARRE WORKOUT", "url": "https://youtu.be/Srrdk9rv3JQ?feature=shared"},
-        {"title": "Day 2 – ", "url": ""},
-        {"title": "Day 3 – ", "url": ""},
-    ],
-    "3 days of English Vocab Vanessa": [
-        {"title": "Day 1 – Education in the US", "url": "https://youtu.be/m8lLwZU9COg?feature=shared"},
-        {"title": "Day 2 – ", "url": ""},
-        {"title": "Day 3 – ", "url": ""},
-    ],
-    "5 days of French Vocab Alice Ayel": [
-        {"title": "Day 1 – Une crêpe délicieuse", "url": "https://youtu.be/fDcOfwBVQsE?feature=shared"},
-        {"title": "Day 2 – Roland, un opéra de Jean-Baptiste Lully", "url": "https://youtu.be/43jIt3dlkdw?feature=shared"},
-        {"title": "Day 3 – La toute petite bonne femme, un conte du bassin", "url": "https://youtu.be/bB6wT2Q6-R8?feature=shared"},
-        {"title": "Day 4 – La cigale et le fourmi, une fable de Jean de La Fontaine", "url": "https://youtu.be/5XmwCBOmJhY?feature=shared"},
-        {"title": "Day 5 – Le loup déguisé en agneau, une fable d'Esope", "url": "https://youtu.be/cJuKJ2SNhGA?feature=shared"},
-    ],
-    "3 days of English YouTube Movies": [
-        {"title": "Day 1 – Winter Spring Summer or Fall", "url": "https://youtu.be/9VTcGNRwIQk?feature=shared"},
-        {"title": "Day 2 – ", "url": ""},
-        {"title": "Day 3 – ", "url": ""},
-    ],
-    "3 days of Podcast Production Pat Flynn": [
-        {"title": "Day 1 – How to Start a Podcast", "url": "https://youtu.be/PIJpOcFf5h4?feature=shared"},
-        {"title": "Day 2 – Top Podcasting Tips & Tools for Recording, Interviews & Exporting", "url": "https://youtu.be/HUwhPlsxpqI?feature=shared"},
-        {"title": "Day 3 – How to Record and Edit a Podcast in Audacity", "url": "https://youtu.be/xl-WDjWrTtk?feature=shared"},
-    ],
-    
+    ],    
 }
