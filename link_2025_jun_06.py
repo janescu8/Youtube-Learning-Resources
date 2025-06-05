45047446032ce897fb78f59806950eee9ca1e1df
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-06 00:38:50 +0800

    Update link_2025_jun_06.py

--- link_2025_jun_06.py
+++ link_2025_jun_06.py
@@ -1,1 +1,8 @@
+# link.py
 
+data = {
+    "2 days of Streamlit": [
+        {"title": "Day 1 – Streamlit Mini Course - Make Websites With ONLY Python", "url": "https://youtu.be/o8p7uQCGD0U?feature=shared"},
+        {"title": "Day 2 – ADVANCED Python AI Multi-Agent Tutorial (RAG, Streamlit, Langflow & More!)", "url": "https://youtu.be/msLovKSj8Q0?feature=shared"},
+    ],  
+}
