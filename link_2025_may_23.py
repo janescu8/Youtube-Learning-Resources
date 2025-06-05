bfca761d6d0623ad5590616242b6cc22e0d41635
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-06 00:41:47 +0800

    Update link_2025_may_23.py

--- link_2025_may_23.py
+++ link_2025_may_23.py
@@ -1,9 +1,6 @@
 # link.py
 
 data = {
-    "1 days of Procreate Production Christiane": [
-        {"title": "Day 1 – Part One | The Fundamentals", "url": "https://youtu.be/Vn8bj0YpZg4?feature=shared"},
-    ],
     "2 days of Piano Production Thomas": [
         {"title": "Day 1 – GOLDEN HOUR", "url": "https://youtu.be/CWlAgyg25OA?feature=shared"},
         {"title": "Day 2 – GOLDEN HOUR", "url": "https://youtu.be/ksZRx9k18Yw?feature=shared"},
