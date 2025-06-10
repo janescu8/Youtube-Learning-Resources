9c102373e14262352702873f545dc59d4150f141
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-10 19:01:01 +0800

    Delete link_2025_jun_06.py

--- link_2025_jun_06.py
+++
@@ -1,7 +0,0 @@
-# link.py
-
-data = {
-    "Japanese Podcast with Hana #12": [
-        {"title": "Day 1 – Habits 習慣🏃‍♀️📚 | Japanese Podcast with Hana #12", "url": "https://youtu.be/ltbRQvkcgfY?start=0&end=616"},
-    ],  
-}
